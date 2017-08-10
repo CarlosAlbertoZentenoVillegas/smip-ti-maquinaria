0c8412033e40a4b5e374bfcd4be3185205f2a4a5
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-08-10 17:27:41 -0500

    Agregar ejemplo de cómo usar jQuery en el ngAfterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as $ from 'jquery';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
+  styleUrls: [ './app.component.css' ]
+} )
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+
+  ngAfterViewInit() {
+    // Como ejemplo estoy navegado a una sección
+    const $elementToGo = $( '#SeccionNosotrosHome' );
+
+    if ($elementToGo.length > 0) {
+      $( 'html,body' )
+        .animate( { scrollTop: $elementToGo.offset().top }, 1000 );
+    }
+
+  }
+
 }
